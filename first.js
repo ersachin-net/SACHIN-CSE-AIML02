8d46e7c47e4d60862de2a62184098ca624918334
Sachin <ersachin.net@gmail.com>
2026-05-30 15:33:30 +0530

    initial commit

---
+++ first.js
@@ -0,0 +1,38 @@
+ 
+    // alert("welcome to my college");
+    // confirm("press ok for blank page");
+   //  prompt("please enter your mobile number");
+   // document.write("hello world");
+  
+   //document.write("hello world");
+   //console.log("sachin Rajput");
+
+   //document.write("hello world<br>");
+   //document.write("hello world");
+   //document.write("<br>");
+   //document.write("CSE");
+
+  // document.write("<div> hello world </div>");
+   //document.write("CSE");
+
+  // document.write("<pre> hello world</pre>");
+  // document.write("CSE");
+
+  //document.write("<p> hello world </p>");
+  //document.write("CSE");
+
+//document.write("<pre> hello world \n AIML2 </pre>");
+
+variable in java script 
+1. var(old method)
+var name 
+
+
+lopp in java sript 
+
+
+
+
+
+
+
